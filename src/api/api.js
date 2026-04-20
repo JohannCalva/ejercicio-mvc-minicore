fb5bec7d99242c731589ac93c3259f7046dc42af
JohannCalva <johanncalvacadme@gmail.com>
2026-04-19 21:30:57 -0500

    Create api.js file with the main functions to interact with supabase

---
+++ src/api/api.js
@@ -0,0 +1,27 @@
+import { supabase } from "../utils/supabase.js"
+
+export async function getVendedores() {
+    const { data, error } = await supabase
+        .from('vendedor')
+        .select('*');
+    if (error) console.error('Error al obtener vendedores:', error);
+    return data;
+}
+
+export async function getVentasPorFecha(fechaInicio, fechaFin) {
+    const { data, error } = await supabase
+        .from('venta')
+        .select('*')
+        .gte('fecha_venta', fechaInicio)
+        .lte('fecha_venta', fechaFin);
+    if (error) console.error('Error al obtener ventas:', error);
+    return data;
+}
+
+export async function getReglas() {
+    const { data, error } = await supabase
+        .from('regla')
+        .select('*');
+    if (error) console.error('Error al obtener reglas:', error);
+    return data;
+}
